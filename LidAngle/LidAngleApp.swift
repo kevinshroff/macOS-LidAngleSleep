c63cfa82a18f80ed547105da47e19fc02eb5f654
deepakness <deepakkr342@gmail.com>
2025-09-11 12:58:55 +0530

    Initial Commit

---
+++ LidAngle/LidAngleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidAngleApp.swift
+//  LidAngle
+//
+//  Created by Deepak Kumar on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LidAngleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LidAngleTests/LidAngleTests.swift
@@ -0,0 +1,17 @@
+//
+//  LidAngleTests.swift
+//  LidAngleTests
+//
+//  Created by Deepak Kumar on 11/09/25.
+//
+
+import Testing
+@testable import LidAngle
+
+struct LidAngleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
